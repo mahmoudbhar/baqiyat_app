c9b98ed632657480daea73b65d560fbee84ad6bd
Mahmoud I.Bhar <mahmoudbhar0@gmail.com>
2026-08-31 15:55:52 +0300

    Add M1A1.png to the application cache

--- sw_v5.js
+++ sw_v5.js
@@ -1,5 +1,5 @@
 const CACHE='baqiyat-v6';
-const APP=['./','./index.html','./manifest.json','./icon.svg'];
+const APP=['./','./index.html','./manifest.json','./M1A1.png'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP)).then(()=>self.skipWaiting()));
 });
